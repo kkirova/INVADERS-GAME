8085f2fdb5b76ed18b9c3732e432e65262522afe
kkirova <52708975+kkirova@users.noreply.github.com>
2019-08-22 14:13:50 +0300

    add player movement

--- scripts/game.js
+++ scripts/game.js
@@ -18,10 +18,28 @@
             this.renderer = new Renderer(this.canvas, this.bounds);
             this.gameObjectFactory = new GameObjectFactory(width, height);
             this.player = this.gameObjectFactory.createPlayer();
+            this._attachGameEvents();
         }
 
         start() {
             this._gameLoop();
+        }
+
+        _attachGameEvents() {
+            window.addEventListener('keydown', (ev) => {
+                this._handleMovementEvent(ev);
+                //this._handleFireEvent(ev);
+            });
+        }
+
+        _handleMovementEvent(ev) {
+            let alpha = 0;
+            if (ev.keyCode === 37) {
+                alpha = -1;
+            } else if(ev.keyCode === 39) {
+                alpha = +1;
+            }
+            this.player.left += alpha * 30;
         }
 
         _gameLoop() {
