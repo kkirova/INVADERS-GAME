0a4242090cd7711b4a60fe2fbae8630d39e45968
kkirova <52708975+kkirova@users.noreply.github.com>
2019-08-22 10:21:25 +0300

    Refactoring

--- scripts/game.js
+++ scripts/game.js
@@ -1,21 +1,7 @@
 (function (scope) {
-    const { renderer } = scope;
-    
-    const init = function (selector, width, height) {
-        const gameContainer = document.querySelector(selector);
-        const canvas = document.createElement('canvas');
-        gameContainer.appendChild(canvas);
-        canvas.width = width;
-        canvas.height = height;
-        const ctx = canvas.getContext('2d');
-        this.bounds = {
-            width,
-            height,
-        };
-
-        this.renderer = new Renderer(ctx, this.bounds);
-        return this;
-    };
+    const {
+        Renderer
+    } = scope;
 
     const dot = {
         left: 0,
@@ -23,31 +9,44 @@
     };
 
     let iteration = 0;
-    const gameLoop = function () {
-        this.renderer.clear();
-        const {
-            left,
-            top
-        } = dot;
-        this.renderer.rendererDot(left, top);
-        const alpha = Math.sin(iteration);
-        iteration++;
-        dot.left += 1;
-        dot.top += alpha * 5;
-        window.requestAnimationFrame(() => {
-            this.gameLoop();
-        });
-    };
 
-    const start = function () {
-        this.gameLoop();
-    };
+    class Game {
+        constructor(selector, width, height) {
+            const gameContainer = document.querySelector(selector);
+            const canvas = document.createElement('canvas');
+            canvas.width = width;
+            canvas.height = height;
+            gameContainer.appendChild(canvas);
+            const ctx = canvas.getContext('2d');
+            this.bounds = {
+                width,
+                height,
+            };
 
-    const game = {
-        init,
-        start,
-        gameLoop
-    };
+            this.renderer = new Renderer(ctx, this.bounds);
+        }
 
-    window.game = game;
+        start() {
+            this._gameLoop();
+        }
+
+        _gameLoop() {
+            this.renderer.clear();
+            const {
+                left,
+                top
+            } = dot;
+
+            this.renderer.rendererDot(left, top);
+            const alpha = Math.sin(iteration);
+            iteration++;
+            dot.left += 1;
+            dot.top += alpha * 5;
+            window.requestAnimationFrame(() => {
+                this._gameLoop();
+            });
+        }
+    }
+    scope.Game = Game;
+
 }(window));
